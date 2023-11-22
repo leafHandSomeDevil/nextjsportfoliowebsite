6a596ce0bb892384e58d1cb70bde33891c16edd1
leafHandSomeDevil <ifortressalex@gmail.com>
2023-11-22 17:47:27 +0800

    changingresendapi

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,5 +1,4 @@
 "use server";
-
 import React from "react";
 import { Resend } from "resend";
 import { validateString, getErrorMessage } from "@/lib/utils";
@@ -11,37 +10,41 @@ export const sendEmail = async (formData: FormData) => {
   const senderEmail = formData.get("senderEmail");
   const message = formData.get("message");
 
+
+
   // simple server-side validation
   if (!validateString(senderEmail, 500)) {
     return {
-      error: "Invalid sender email",
-    };
-  }
-  if (!validateString(message, 5000)) {
-    return {
-      error: "Invalid message",
-    };
+      error: "Invalid sender email"
+    }
   }
 
+  if (!validateString(message, 5000)) {
+    return {
+      error: "Invalid message"
+    }
+  }
   let data;
   try {
     data = await resend.emails.send({
       from: "Contact Form <onboarding@resend.dev>",
-      to: "bytegrad@gmail.com",
+      to: "ifortressalex@gmail.com",
       subject: "Message from contact form",
-      reply_to: senderEmail,
+      reply_to: senderEmail as string,
       react: React.createElement(ContactFormEmail, {
-        message: message,
-        senderEmail: senderEmail,
-      }),
+        message: message as string,
+        senderEmail: senderEmail as string,
+      })
+
     });
   } catch (error: unknown) {
     return {
-      error: getErrorMessage(error),
+      error: getErrorMessage(error)
     };
-  }
 
+  }
   return {
     data,
   };
 };
+
--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Alex | Personal Portfolio",
-  description: "Ricardo is a full-stack developer with 8 years of experience.",
+  description: "Alex is a React developer with 5 years of experience.",
 };
 
 export default function RootLayout({
